608123aa6191d26c4e3c612744ca6d141f28ad3a
Vasanth Kumar <vasanthbillov@gmail.com>
2023-04-18 15:34:19 +0000

    Added few more file

---
+++ Testfile.py
@@ -0,0 +1,7 @@
+class vehicle():
+    def __init__(self,_name):
+        print('init')
+        self.name =_name
+        
+    def print_name(self):
+        print(self.name)
--- new_feature.py
+++ new_feature.py
@@ -1,4 +1,7 @@
-
+import Testfile
 
 def call():
     print('new feature function called')
+    
+    car = Testfile.vehicle("maruti")
+    car.print_name()
